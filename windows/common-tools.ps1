5807d028c4690f5e9271bf8c9f49be424fb89994
JBrLloyd <jacksonbrlloyd@gmail.com>
2021-06-03 20:10:34 +1000

    adding powershell modules

--- windows/common-tools.ps1
+++ windows/common-tools.ps1
@@ -1,3 +1,5 @@
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
 choco install -y 7zip.install
+
+Install-Module posh-git oh-my-posh PSReadLine
