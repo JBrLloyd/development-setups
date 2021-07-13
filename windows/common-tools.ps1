bb4c5232f4be1ad95bafd26b67fcd4a0b9c39304
JBrLloyd <jacksonbrlloyd@gmail.com>
2021-07-13 10:50:53 +1000

    Update common-tools.ps1

--- windows/common-tools.ps1
+++ windows/common-tools.ps1
@@ -1,5 +1,6 @@
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
 choco install -y 7zip.install
+choco install -y opera firefox ServiceBusExplorer
 
 Install-Module posh-git oh-my-posh PSReadLine
